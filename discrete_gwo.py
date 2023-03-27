96f402db49c7f80c4f48f1d0a47dfb8dd5976bef
Sebastián Cáceres Gelvez <63064790+scaceresg@users.noreply.github.com>
2023-03-27 21:55:22 +0100

    Update discrete_gwo

--- discrete_gwo.py
+++ discrete_gwo.py
@@ -11,11 +11,19 @@ class GWODiscreto(ModeloUAFLP):
 
     # Correr algoritmo DGWO
     def correr_dgwo(self, tam_manada:int, theta_1:float, theta_2:float):
+        
+        
+        # Construir el cuerpo del algoritmo: iteraciones o no_mejoras despues de tantas veces
         self.inicializar_manada(tam_manada)
         self.identificar_lideres()
-        
-        for lobo in self.manada:
-            self.buscar_presa(theta_1, lobo)
+        self.buscar_presa(theta_1)
+        self.atacar_presa(theta_2)
+        self.reunir_lideres()
+
+        # Construir return
+        # p_min = np.argmin(self.fit_lideres)
+        # return self.manada[p_min], self.fit_manada[p_min]
+
 
     # Inicializar la manada
     def inicializar_manada(self, tam_manada):
@@ -52,35 +60,132 @@ class GWODiscreto(ModeloUAFLP):
 
         return self.lobos_lideres, self.fit_lideres
 
-    # Busqueda de la presa
-    def buscar_presa(self, theta_1, lobo):
-
-        tam_mov = np.floor(self.n_dptos * theta_1).astype(int)
-        for elem in lobo:
-            pos_inicial = np.random.randint(0, self.n_dptos)
-
+    # Comportamiento de busqueda de la presa
+    def buscar_presa(self, theta_1):
         
-        # corte = np.random.randint(0, self.n_dptos + 1 - movimiento)
-        # posiciones = np.arange(corte, corte + movimiento)
+        # Tamaño del movimiento a realizar: ej. 2 posiciones
+        tam_mov = np.floor(self.n_dptos * theta_1).astype(int)
+        
+        for ind, lobo in enumerate(self.manada):
+            
+            # Encontrar índices del movimiento
+            pos_mov, pos_fin = self.encontrar_pos(tam_mov)
 
-        # for ind, lobo in enumerate(self.manada):
+            # Realizar movimiento para cada elemento de lobo
+            lobo_dp = lobo[0]
+            lobo_dp = self.realizar_movimiento(lobo_dp, pos_mov, pos_fin)
 
-        #     # Individuo departamentos
-        #     seccion = lobo[posiciones[0]:posiciones[-1]+1]
-        #     lobo = np.delete(lobo, posiciones)
+            lobo_bh = lobo[1]
+            lobo_bh = self.realizar_movimiento(lobo_bh, pos_mov, pos_fin)
+            if lobo_bh[-1] == 0:
+                lobo_bh[-1] = 1 # El elemento de bahías debe terminar en 1
+            
+            nueva_pos_lobo = [lobo_dp, lobo_bh]
+            nueva_pos_fit = self.calcular_fitness(nueva_pos_lobo)
 
-        #     num_rand = np.random.random(size=1)
+            # Comparar lobo con alpha
+            if nueva_pos_fit < min(self.fit_lideres):
+                self.manada[ind] = self.lobos_lideres[0] # Reemplazar lobo con el alpha en manada
+                self.fit_manada[ind] = self.fit_lideres[0]
 
-        #     if corte < self.n_dptos/2:
-        #         lobo = np.append(lobo, seccion)
-        #     else:
-        #         lobo = np.append(seccion, lobo)
+                self.lobos_lideres[0] = nueva_pos_lobo # Reemplazar alpha con nuevo lobo
+                self.fit_lideres[0] = nueva_pos_fit
+                break # Salirse del for
+            elif nueva_pos_fit < self.fit_manada[ind]:
+                self.manada[ind] = nueva_pos_lobo
+                self.fit_manada[ind] = nueva_pos_fit
 
-        #     self.manada.pop(ind)
-        #     self.fit_manada.pop(ind)
-        #     self.manada.append(lobo)
-        #     self.fit_manada.append(self.calcular_fitness(lobo))
+    # Encontrar posiciones de movimiento
+    def encontrar_pos(self, tam_movimiento):
+        
+        pos_inicial, pos_final = np.random.randint(0, self.n_dptos + 1 - tam_movimiento, 2) # Índices inicial (ej. 1) y donde se ubicará el nuevo movimiento (ej. 5)
+        pos_movimiento = np.arange(pos_inicial, pos_inicial + tam_movimiento) # Índices del movimiento (ej. [1, 2])
 
-            # Sebas, estoy en el metodo de buscar_presa. 
-            # Me encontre con el problema que `lobo in manada` es una lista de dos elementos [[1, 2, 3..], [0, 1, 0,...1]]
-            # Necesito identificar un metodo para realizar el movimiento para ambos elementos, sobretodo el de [0, 1, 0, ...1]
+        return pos_movimiento, pos_final
+
+    # Realizar movimiento en el proceso de búsqueda
+    def realizar_movimiento(elem_lobo, pos_movimiento, pos_final):
+        
+        movimiento = elem_lobo[pos_movimiento]
+        elem_lobo = np.delete(elem_lobo, pos_movimiento)
+        elem_lobo = np.insert(elem_lobo, pos_final, movimiento)
+
+        return elem_lobo
+    
+    # Comportamiento de ataque de la presa
+    def atacar_presa(self, theta_2):
+
+        da = np.floor(self.n_dptos * theta_2)
+
+        for ind_md, lobo in enumerate(self.manada):
+            
+            nuevo_atq_lobo = []
+            
+            # Seleccionar lobo lider: alpha, beta o delta
+            lider = self.seleccionar_lider()
+
+            # Calcular distancia y acercar a presa (lobo lider)
+            for ind, elem in enumerate(lobo):
+                distancia = self.calcular_dist(elem, lider[ind])
+                i = 0
+                while distancia > da:
+                    elem, i = self.reducir_dist(elem, lider[ind], i)
+                    distancia = self.calcular_dist(elem, lider[ind])
+                
+                nuevo_atq_lobo.append(elem)
+
+            nuevo_atq_fit = self.calcular_fitness(nuevo_atq_lobo)
+
+            # Si fitness es mejor que lobo actual: actualizar
+            if nuevo_atq_fit < self.fit_manada[ind_md]:
+                self.manada[ind_md] = nuevo_atq_lobo
+                self.fit_manada[ind_md] = nuevo_atq_fit
+        
+        return self.manada, self.fit_manada
+
+    # Seleccionar lider para calcular distancia
+    def seleccionar_lider(self):
+
+        rand = np.random.random()
+        if rand < 0.5:
+            lider = self.lobos_lideres[0] # Calcular con alpha (50% prob)
+        elif rand < 0.75:
+            lider = self.lobos_lideres[1] # Calcular con beta (25% prob)
+        else:
+            lider = self.lobos_lideres[2] # Calcular con delta (25% prob)
+        
+        return lider
+     
+    # Calcular distancia entre elementos de lobos
+    def calcular_dist(self, elem_lobo, elem_lider):
+
+        dist = 0
+        for ind in range(len(elem_lider)):
+            if elem_lider[ind] != elem_lobo[ind]:
+                dist += 1
+
+        return dist
+    
+    # Reducir distancia entre elementos de lobos
+    def reducir_dist(self, elem_lobo, elem_lider, index):
+
+        for i in self.departamentos[index:]:
+            lider_val = elem_lider[i-1]
+            lobo_val = elem_lobo[i-1]
+            if lider_val != lobo_val:
+                pos_cambio = np.where(elem_lobo == lider_val)
+                elem_lobo[i-1] = lider_val
+                elem_lobo[pos_cambio] = lobo_val
+
+                return elem_lobo, i
+            
+    # Reunir lideres a la manada para reiniciar iteracion
+    def reunir_lideres(self):
+        
+        while len(self.lobos_lideres) > 0:
+            
+            pos_min = np.argmin(self.fit_lideres)
+            self.manada.append(self.lobos_lideres[pos_min])
+            self.fit_manada.append(self.fit_lideres[pos_min])
+        
+        return self.manada, self.fit_manada

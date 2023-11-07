7128442302eaffb4f0fe2986904cd143f78c1f84
Sebastián Cáceres Gelvez <63064790+scaceresg@users.noreply.github.com>
2023-11-07 13:33:29 +0000

    Update main_uaflp.py and create Aplication-DGWO-empresa.ipynb

--- main_uaflp.py
+++ main_uaflp.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 from alg_lobo_gris import AlgLoboGris
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -29,7 +30,7 @@ for ln in data:
     inst = ln.split()[0]
 
     lobo_gris = AlgLoboGris(archivo_datos=inst)
-
+    
     resultados = {}
     resultados['set_params'] = []
 
@@ -39,6 +40,8 @@ for ln in data:
 
     for ind, params in enumerate(set_params):
         
+        fig = plt.figure(dpi=400)
+
         print(f'Corriendo set de parámetros {ind} = {params}')
         resultados[f'set_params'].append(f'set_params_{ind}')
 
@@ -49,6 +52,8 @@ for ln in data:
 
             lobo_res = lobo_gris.optimizacion_lobo_gris(tam_manada=t_manada, n_lideres=n_lideres, theta_1=theta_1,
                                                         theta_2=theta_2, theta_3=theta_3, tiempo_lim=t_limite)
+            
+            plt.plot(lobo_res['vals_fitness'], label=f'round: {rnd}')
 
             resultados[f'UB_{rnd}'].append(lobo_res['vals_fitness'][-1])
             resultados[f'SOL_{rnd}'].append(lobo_res['sols_uaflp'][-1])
@@ -56,6 +61,13 @@ for ln in data:
             print(f'Finalizando corrida de set de parámetros {ind} para la instancia {inst} en la ronda {rnd}')
 
             rnd += 1
+
+        plt.xlabel('Iterations')
+        plt.ylabel('Fitness function value')
+        plt.title('Fitness function value per round')
+        plt.legend()
+
+        plt.savefig(f'plot-application-set-params-{ind}.png')
 
     df = pd.DataFrame(resultados)
     df.set_index('set_params', inplace=True)    
